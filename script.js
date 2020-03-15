31060921afbcc011b853d9f65126a10e86611180
wuyaheng <wuyaheng2016@gmail.com>
2020-03-15 13:08:14 -0400

    fix the distorted png

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function updateForecastWeather(ForecastData){
             var Ftemp_f = $("<p>").addClass("card-text").text("Temperature: " + ((Number(JSON.stringify(ForecastData.list[i].main.temp)) - 273.15) * 9/5 + 32).toFixed(2) + " °F");
             var humid_f = $("<p>").addClass("card-text").text("Humidity: " + JSON.stringify(ForecastData.list[i].main.humidity) + "%");
             var wind_f = $("<p>").addClass("card-text mb-2").text("Wind Speed: " + JSON.stringify(ForecastData.list[i].wind.speed) + " MPH");
-            var img_f = $("<img>").attr("src","https://openweathermap.org/img/wn/" + ForecastData.list[i].weather[0].icon + "@2x.png");
+            var img_f = $("<img>").addClass("h-100").attr("src","https://openweathermap.org/img/wn/" + ForecastData.list[i].weather[0].icon + "@2x.png").addClass("fixImg");
             var Datef = $("<p>").addClass("card-text text-center mt-2").text(JSON.parse(JSON.stringify(ForecastData.list[i].dt_txt)).substring(0, 10));
             col.append(Datef, img_f, Ftemp_f, humid_f, wind_f);
             $("#forecastRowContainer").append(col);
@@ -45,7 +45,7 @@ function updateCurrentWeather(Data) {
     var Ftemp = $("<p>").addClass("card-text").text("Temperature: " + ((Number(JSON.stringify(Data.main.temp)) - 273.15) * 9/5 + 32).toFixed(2) + " °F");
     var humid = $("<p>").addClass("card-text").text("Humidity: " + JSON.stringify(Data.main.humidity) + "%");
     var wind = $("<p>").addClass("card-text").text("Wind Speed: " + JSON.stringify(Data.wind.speed) + " MPH");
-    var img = $("<img>").attr("src","https://openweathermap.org/img/wn/" + Data.weather[0].icon + "@2x.png");
+    var img = $("<img>").attr("src","https://openweathermap.org/img/wn/" + Data.weather[0].icon + "@2x.png"); 
     // var currentDate = moment().format('MMMM Do YYYY, h:mm A')  
     // $(".card-header").html("<div>").append(currentDate);
     $("#todayContainer").html("<div>").append(title, img, Ftemp, humid, wind); 
