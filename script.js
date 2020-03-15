4d328e6ff38513b681222047f89eb3e1c3f5e7e0
wuyaheng <wuyaheng2016@gmail.com>
2020-03-15 12:31:30 -0400

    change http to https

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $("#search-button").on("click", function(event) {
     // build the query url for the ajax request to the open weather API
     var searchValue = $("#search-value").val();
     var apiKey = "76093c5b76715cabd5992fc44d0a2e7e"
-    var queryCurrentURL = "http://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&APPID=" + apiKey;
+    var queryCurrentURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&APPID=" + apiKey;
     // make ajax request to the API and GET JSON data
     // data then passed as an argument to the updatePage function
     $.ajax({
@@ -14,7 +14,7 @@ $("#search-button").on("click", function(event) {
     }).then(updateCurrentWeather);
 
     //api.openweathermap.org/data/2.5/forecast?q
-    var queryForecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&APPID=" + apiKey;
+    var queryForecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&APPID=" + apiKey;
     $.ajax({
         url: queryForecastURL,
         method: "GET"
@@ -29,7 +29,7 @@ function updateForecastWeather(ForecastData){
             var Ftemp_f = $("<p>").addClass("card-text").text("Temperature: " + ((Number(JSON.stringify(ForecastData.list[i].main.temp)) - 273.15) * 9/5 + 32).toFixed(2) + " °F");
             var humid_f = $("<p>").addClass("card-text").text("Humidity: " + JSON.stringify(ForecastData.list[i].main.humidity) + "%");
             var wind_f = $("<p>").addClass("card-text mb-2").text("Wind Speed: " + JSON.stringify(ForecastData.list[i].wind.speed) + " MPH");
-            var img_f = $("<img>").attr("src","http://openweathermap.org/img/wn/" + ForecastData.list[i].weather[0].icon + "@2x.png");
+            var img_f = $("<img>").attr("src","https://openweathermap.org/img/wn/" + ForecastData.list[i].weather[0].icon + "@2x.png");
             var Datef = $("<p>").addClass("card-text text-center mt-2").text(JSON.parse(JSON.stringify(ForecastData.list[i].dt_txt)).substring(0, 10));
             col.append(Datef, img_f, Ftemp_f, humid_f, wind_f);
             $("#forecastRowContainer").append(col);
@@ -45,7 +45,7 @@ function updateCurrentWeather(Data) {
     var Ftemp = $("<p>").addClass("card-text").text("Temperature: " + ((Number(JSON.stringify(Data.main.temp)) - 273.15) * 9/5 + 32).toFixed(2) + " °F");
     var humid = $("<p>").addClass("card-text").text("Humidity: " + JSON.stringify(Data.main.humidity) + "%");
     var wind = $("<p>").addClass("card-text").text("Wind Speed: " + JSON.stringify(Data.wind.speed) + " MPH");
-    var img = $("<img>").attr("src","http://openweathermap.org/img/wn/" + Data.weather[0].icon + "@2x.png");
+    var img = $("<img>").attr("src","https://openweathermap.org/img/wn/" + Data.weather[0].icon + "@2x.png");
     // var currentDate = moment().format('MMMM Do YYYY, h:mm A')  
     // $(".card-header").html("<div>").append(currentDate);
     $("#todayContainer").html("<div>").append(title, img, Ftemp, humid, wind); 
