e778099686dec4eed9fc8ca66984ef2d7dbab580
wuyaheng <wuyaheng2016@gmail.com>
2021-03-28 00:55:03 -0400

    add map

--- script.js
+++ script.js
@@ -1,48 +1,64 @@
-
-
 var userCityArray = JSON.parse(localStorage.getItem("userCityArray")) || [];
+const base_url = "https://maps.googleapis.com/maps/api/geocode/json"
+var historyObj = {}
+var MAP_CONTAINER = document.getElementById('mapidContainer')
 
 localStorage.clear();
 
-// add click event to search button
-
 $("#search-button").on("click", function(event) {
-    // prevent page from reloading on form submit
     event.preventDefault();
-    // build the query url for the ajax request to the open weather API
     var searchValue = $("#search-value").val();
     userCityArray.push(searchValue);
  
     localStorage.setItem("userCityArray", JSON.stringify(userCityArray));
 
     for (var i = 0; i < userCityArray.length; i++) {
-        var cityButton = $('<button/>').addClass("btn btn-light d-inline historyItem mr-1 mt-1").text(userCityArray[i]);
-    }
+        var cityButton = $('<button/>')
+        .addClass("btn btn-light d-inline historyItem mr-1 mt-1")
+        .text(userCityArray[i])
+        .click(() => {
+            getWeatherForCity(searchValue)
+        })
+    } 
     $(".history").append(cityButton);
-
-
-
-
-    var apiKey = "76093c5b76715cabd5992fc44d0a2e7e"
-    var queryCurrentURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&APPID=" + apiKey;
-    // make ajax request to the API and GET JSON data
-    // data then passed as an argument to the updatePage function
-    $.ajax({
-        url: queryCurrentURL,
-        method: "GET"
-    }).then(updateCurrentWeather);
-
-    //api.openweathermap.org/data/2.5/forecast?q
-    var queryForecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&APPID=" + apiKey;
-    $.ajax({
-        url: queryForecastURL,
-        method: "GET"
-    }).then(updateForecastWeather);
+    getWeatherForCity(searchValue)
 });
 
-function updateForecastWeather(ForecastData){
+
+function getWeatherForCity(name) {
+    if(historyObj[name]) {
+        console.log("first function")
+        updateCurrentWeather(name);
+        console.log("second function") 
+        updateForecastWeather(name) 
+    } else {
+        var queryCurrentURL = "https://api.openweathermap.org/data/2.5/weather?q=" + name + "&APPID=" + apiKey;
+        $.ajax({
+            url: queryCurrentURL,
+            method: "GET"
+        }).then(data => {
+            console.log("fetched data for current", data)
+            addToHistoryObj(data.name, "current", data)
+            updateCurrentWeather(data.name)
+        });
+    
+        var queryForecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + name + "&APPID=" + apiKey;
+        $.ajax({
+            url: queryForecastURL,
+            method: "GET"
+        }).then(data => {
+            console.log("fetched data for forecast", data) 
+            addToHistoryObj(data.city.name, "forecast", data)
+            updateForecastWeather(data.city.name) 
+        });
+    }
+}
+
+function updateForecastWeather(name){
+    console.log("update forecast weather") 
+    const ForecastData = historyObj[name].forecast 
     $("#forecastContainer").html("<div>").append("<div class='row justify-content-around text-justify' id='forecastRowContainer'>");
-    for(var i = 0; i < ForecastData.list.length; i++) {
+    for(var i = 0; i < ForecastData.list.length; i++) { 
         if(ForecastData.list[i].dt_txt.indexOf("15:00:00") !== -1) {
             var col = $("<div>").addClass("forecast-div").addClass("col-md-2").addClass("card mb-3").addClass("cardBackground");
             var Ftemp_f = $("<p>").addClass("card-text").text("Temperature: " + ((Number(JSON.stringify(ForecastData.list[i].main.temp)) - 273.15) * 9/5 + 32).toFixed(2) + " °F");
@@ -58,20 +74,50 @@ function updateForecastWeather(ForecastData){
 }
 
 
-function updateCurrentWeather(Data) {
-    var title = $("<h3>").addClass("card-title text-center").text(JSON.parse(JSON.stringify(Data.name)));
-   
+function updateCurrentWeather(cityName) {
+    console.log("update current weather")  
+    var data = historyObj[cityName].current
+    var title = $("<h3>").addClass("card-title text-center").text(JSON.parse(JSON.stringify(data.name)));
     // (K − 273.15) × 9/5 + 32 = °F
-    var Ftemp = $("<p>").addClass("card-text").text("Temperature: " + ((Number(JSON.stringify(Data.main.temp)) - 273.15) * 9/5 + 32).toFixed(2) + " °F");
-    var humid = $("<p>").addClass("card-text").text("Humidity: " + JSON.stringify(Data.main.humidity) + "%");
-    var wind = $("<p>").addClass("card-text").text("Wind Speed: " + JSON.stringify(Data.wind.speed) + " MPH");
-    var img = $("<img>").attr("src","https://openweathermap.org/img/wn/" + Data.weather[0].icon + "@2x.png").addClass("rounded mx-auto d-block"); 
-    var descr = $("<p>").addClass("card-text text-center textEffect").text(JSON.parse(JSON.stringify(Data.weather[0].description)));
+    var Ftemp = $("<p>").addClass("card-text").text("Temperature: " + ((Number(JSON.stringify(data.main.temp)) - 273.15) * 9/5 + 32).toFixed(2) + " °F");
+    var humid = $("<p>").addClass("card-text").text("Humidity: " + JSON.stringify(data.main.humidity) + "%");
+    var wind = $("<p>").addClass("card-text").text("Wind Speed: " + JSON.stringify(data.wind.speed) + " MPH");
+    var img = $("<img>").attr("src","https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png").addClass("rounded mx-auto d-block"); 
+    var descr = $("<p>").addClass("card-text text-center textEffect").text(JSON.parse(JSON.stringify(data.weather[0].description)));
     // var currentDate = moment().format('MMMM Do YYYY, h:mm A')  
     // $(".card-header").html("<div>").append(currentDate); 
     $("#todayContainer").html("<div>").append(title, img, descr, Ftemp, humid, wind); 
+
+
+    MAP_CONTAINER.innerHTML = "" 
+    const MAP_ID = document.createElement("div");
+    MAP_ID.setAttribute("id", "mapid");
+    MAP_CONTAINER.appendChild(MAP_ID);
+    var mymap = L.map('mapid').setView([40, -97], 4);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: apiKeyMap
+    }).addTo(mymap);
+
+
+    for(var key in historyObj) {
+        var ele = historyObj[key]
+        L.marker([ele.current.coord.lat, ele.current.coord.lon]).addTo(mymap).bindTooltip(ele.current.name)
+    }
 }
 
+function addToHistoryObj(city, dataType, data) {
+    if (!historyObj[city]) { 
+        historyObj[city] = {}
+        console.log(historyObj)
+    }
+    historyObj[city][dataType] = data 
+}
 
 
 
