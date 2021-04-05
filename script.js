3d9cff6b79da23f301699774bbe83fcc09028d0a
wuyaheng <wuyaheng2016@gmail.com>
2021-04-05 10:57:44 -0400

    add additional map tilelayer

--- script.js
+++ script.js
@@ -149,26 +149,27 @@ function updateCurrentWeather(cityName) {
 
     var screenWidth = document.documentElement.clientWidth;
     if (Number(screenWidth) < 768) {
-        var mymap = L.map('mapid').setView([40, -97],3);
+        var mymap = L.map('mapid', {zoomControl: false}).setView([40, -97],3);
+        L.tileLayer(
+            "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+              attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+              maxZoom: 18,
+              id: "mapbox/streets-v10",
+              tileSize: 512,
+              zoomOffset: -1,
+              accessToken: apiKeyMap,
+            }
+          ).addTo(mymap);
     } else {
         var mymap = L.map('mapid', {zoomControl: false}).setView([40, -97],4);
+        L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
+            maxZoom: 20,
+            attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+        }).addTo(mymap);        
     }
 
-// L.tileLayer(
-//     "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-//       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//       maxZoom: 18,
-//       id: "mapbox/streets-v10",
-//       tileSize: 512,
-//       zoomOffset: -1,
-//       accessToken: apiKeyMap,
-//     }
-//   ).addTo(mymap);
 
-  L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-}).addTo(mymap);
+
 
 
 
