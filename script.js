d7b1b551b7d9bc2b147b9a8c9397293cf2612dfe
wuyaheng <wuyaheng2016@gmail.com>
2021-04-04 22:17:52 -0400

    update map tile

--- script.js
+++ script.js
@@ -141,11 +141,16 @@ function updateCurrentWeather(cityName) {
         var mymap = L.map('mapid', {zoomControl: false}).setView([40, -97],4);
     }
 
-
-    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-}).addTo(mymap);
+L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: "mapbox/streets-v11",
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: apiKeyMap,
+    }
+  ).addTo(mymap);
 
 
     var pinIcon = L.icon({
